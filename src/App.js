2e00b8298ff1194c6a312eb52f2e71fb7bcdafe3
phamhoangduy207 <79884923+phamhoangduy207@users.noreply.github.com>
2021-12-01 17:24:39 +0700

    Timer that can change color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Time from "./Time/Time";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: [],
+      DataisLoad: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      );
+
+    return (
+      <div className="App">
+      <Time />
+        <h1> Users </h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Full name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.username}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Time/Time.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../Time/Time.css";
+
+class Time extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date(), color: "red" };
+    this.changeColor = this.changeColor.bind(this)
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+    document.title = `${this.state.color}`;
+  }
+  componentDidUpdate(){
+    document.title = `${this.state.color}`;
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  changeColor(){
+    const newColor = this.state.color === "red" ? "blue" : "red"
+    this.setState({
+      color: newColor
+    })
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <h2 onClick = {this.changeColor} className="time-red" id="time" style={{ color: this.state.color }}>
+          {this.state.date.toLocaleTimeString()}
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Time;
