4c455bd5b95f405f4b5a4e086ff93137654d82a3
phamhoangduy207 <79884923+phamhoangduy207@users.noreply.github.com>
2021-12-06 17:54:52 +0700

    using hooks

--- src/App.js
+++ src/App.js
@@ -1,64 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Time from "./Time/Time";
+import Time from "./Components/Time/Time";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const axios = require('axios');
 
-    this.state = {
-      item: [],
-      DataisLoad: false,
-    };
+function App(props) {
+  const [data, setData] = useState({
+    items: [],
+    isLoaded: false,
+  });
+
+  async function getData(){
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      console.log(response.data);
+      setData({
+        items: response.data,
+        isLoaded: true
+      })
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
+  useEffect(() => {
+    getData()
+  }, []);
 
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      );
-
+  const DataisLoaded  = data.isLoaded;
+  if (!DataisLoaded)
     return (
-      <div className="App">
-      <Time />
-        <h1> Users </h1>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Username</th>
-              <th>Full name</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.username}</td>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div>
+        <h1>Loading...</h1>
       </div>
-      );
-  }
+    );
+
+  return (
+    <div className="App">
+      <Time />
+      <h1> Users </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Full name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.items.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.username}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
